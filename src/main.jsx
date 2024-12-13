9a662b821dad43231b666e169bcda59cf59882f9
Aashish17405 <aashish17405@gmail.com>
2024-12-13 10:38:20 +0530

    readme file added

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import ResultPage from './pages/ResultPage.jsx';
-import OverviewPage from './pages/OverviewPage.jsx'
+import OverviewPage from './pages/OverviewPage.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <Router>
+    <Analytics />
     <Routes>
       <Route Component={App} path='/'/>
       <Route Component={ResultPage} path='/result_page'/>
